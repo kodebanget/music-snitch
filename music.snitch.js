3960e25f0deb2fe9bbcef2b6469d9924d7f683d3
kodebanget <151415765+kodebanget@users.noreply.github.com>
2025-07-05 16:32:15 +0700

    Update music.snitch.js

--- music.snitch.js
+++ music.snitch.js
@@ -4,7 +4,7 @@ class MusicSnitch extends HTMLElement {
   }
 
   connectedCallback(e) {
-    const URL = 'https://1443203a597611f0a07af69ea79377d9.val.run' // my personal proxy
+    const URL = 'https://1443203a597611f0a07af69ea79377d9.val.run/' // my personal proxy
     fetch(URL).then(response => response.json()).then((data) => {
       const track = data.recenttracks.track[0]
 
